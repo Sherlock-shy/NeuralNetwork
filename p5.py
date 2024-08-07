cb6d1adda6aea14c4b21ae077f6b68a702050f1a
Ho Yin Siu <hohobee12@gmail.com>
2024-08-07 18:10:40 +0100

    Softmax Activation

--- p5.py
+++ p5.py
@@ -4,12 +4,6 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -21,17 +15,24 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-#https://cs231n.github.io/neural-networks-case-study/
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
-layer1 = Layer_Dense(2, 5)
-#layer2 = Layer_Dense(5, 2)
+X, y = spiral_data(samples = 100, classes = 3)
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-print(layer1.output)
-activation1.forward(layer1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
 
-print(activation1.output)
-#print(layer1.output)
-#layer2.forward(layer1.output)
-#print(layer2.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
