d864c4ecf8b6ab39c4800d9f4912babb2bf92475
Ho Yin Siu <hohobee12@gmail.com>
2024-08-07 16:54:09 +0100

    hidden layer

--- p1.py
+++ p1.py
@@ -6,7 +6,6 @@ X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -16,7 +15,7 @@ class Layer_Dense:
 
 layer1 = Layer_Dense(4, 5)
 layer2 = Layer_Dense(5, 2)
-
+ 
 layer1.forward(X)
 #print(layer1.output)
 layer2.forward(layer1.output)
