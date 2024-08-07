5b68812ec333dd088fea4a9e1c51a0987bf47fda
Ho Yin Siu <hohobee12@gmail.com>
2024-08-07 17:28:40 +0100

    Hidden Layer Activation Functions

--- p5.py
+++ p5.py
@@ -1,15 +1,37 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, -100]
-output = []
+X, y = spiral_data(100, 3)
 
-for i in inputs:
-    output.append(max(0, i))
-    
-print(output)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+#https://cs231n.github.io/neural-networks-case-study/
+
+layer1 = Layer_Dense(2, 5)
+#layer2 = Layer_Dense(5, 2)
+activation1 = Activation_ReLU()
+
+layer1.forward(X)
+print(layer1.output)
+activation1.forward(layer1.output)
+
+print(activation1.output)
+#print(layer1.output)
+#layer2.forward(layer1.output)
+#print(layer2.output)
